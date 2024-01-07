f03f7184f824349018e76cb9b3eaf6e9fe338db7
Erdem KÖSTEK <84468790+ErdemGou@users.noreply.github.com>
2024-01-07 05:46:31 +0300

    11.Gün 3:02:30

---
+++ src/layouts/CartSummary.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {
+  DropdownMenu,
+  DropdownItem,
+  MenuMenu,
+  MenuItem,
+  Button,
+  Dropdown,
+  Menu,
+  Container,
+} from "semantic-ui-react";
+
+export default function CartSummary() {
+  return (
+    <div>
+      <Dropdown item text="Language">
+        <DropdownMenu>
+          <DropdownItem>English</DropdownItem>
+          <DropdownItem>Russian</DropdownItem>
+          <DropdownItem>Spanish</DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+    </div>
+  );
+}
